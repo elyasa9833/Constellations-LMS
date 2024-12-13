d3e506886a0827f409a57cc344c745603546f49b
Amri Elyasa <aelyasa09@gmail.com>
2024-12-13 10:35:31 +0700

    refactor: update DatabaseSeeder to use CourseCategorySeeder for seeding course categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,9 +5,10 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Course;
-use App\Models\CourseCategory;
 use App\Models\CourseChapter;
+use App\Models\CourseCategory;
 use Illuminate\Database\Seeder;
+use Database\Seeders\CourseCategorySeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,7 +18,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(5)->create();
-        CourseCategory::factory(5)->create();
+        $this->call([CourseCategorySeeder::class]);
         Course::factory(10)->create();
         CourseChapter::factory(20)->create();
 
